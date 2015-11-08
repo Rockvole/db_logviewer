1e232fcf894a8fef3334d6892261a21ac11e12a6
Rockvole <rockvole@gmail.com>
2015-11-08 13:36:31 -0800

    Shuffle ShowLogBackViewPort

--- src/com/rockvole/logback/ShowLogBackViewPort.java
+++ src/com/rockvole/logback/ShowLogBackViewPort.java
@@ -69,105 +69,7 @@ public class ShowLogBackViewPort {
 
     }
 
-    private static Map<Long, LogbackTableStruct> fetchRows(Long currentTs, int linesToShow) {
-
-        Map<Long, LogbackTableStruct> map = new TreeMap<Long,LogbackTableStruct>();
-        String sql = "   SELECT timestmp, " +
-                     "          formatted_message, " +
-                     "          logger_name, " +
-                     "          level_string, " +
-                     "          thread_name, " +
-                     "          reference_flag, " +
-                     "          arg0, " +
-                     "          arg1, " +
-                     "          arg2, " +
-                     "          arg3, " +
-                     "          caller_filename, " +
-                     "          caller_class, " +
-                     "          caller_method, " +
-                     "          caller_line, " +
-                     "          event_id " +
-                     "     FROM logging_event ";
-        if(currentTs!=null) {
-            sql +=   "    WHERE timestmp <= " + currentTs;
-        }
-        sql +=       " ORDER BY timestmp desc" +
-                     "    LIMIT " + linesToShow;
-        System.out.println(sql);
-
-        try {
-            Connection conn = fetchDataSource().getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-
-            ResultSet rs = ps.executeQuery();
-            LogbackTableStruct struct;
-            while(rs.next()) {
-                struct = new LogbackTableStruct(rs.getLong("TIMESTMP"),
-                                                rs.getString("FORMATTED_MESSAGE"),
-                                                rs.getString("LOGGER_NAME"),
-                                                rs.getString("LEVEL_STRING"),
-                                                rs.getString("THREAD_NAME"),
-                                                rs.getShort("REFERENCE_FLAG"),
-                                                rs.getString("ARG0"),
-                                                rs.getString("ARG1"),
-                                                rs.getString("ARG2"),
-                                                rs.getString("ARG3"),
-                                                rs.getString("CALLER_FILENAME"),
-                                                rs.getString("CALLER_CLASS"),
-                                                rs.getString("CALLER_METHOD"),
-                                                rs.getString("CALLER_LINE"),
-                                                rs.getInt("EVENT_ID"));
-
-                map.put(struct.ts, struct);
-            }
-        } catch(SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        return map;
-    }
-
-    private static Long getPreviousTs(Long currentTs, int limit) {
-        Long previousTs=null;
-        String sql = "   SELECT timestmp " +
-                "     FROM logging_event " +
-                "    WHERE timestmp < " + currentTs +
-                " ORDER BY timestmp desc" +
-                "    LIMIT "+limit;
-        try {
-            Connection conn = fetchDataSource().getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()) {
-                previousTs = rs.getLong("TIMESTMP");
-            }
-        } catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return previousTs;
-    }
-
-    private static Long getNextTs(Long currentTs, int limit) {
-        Long nextTs=null;
-        String sql = "   SELECT timestmp " +
-                "     FROM logging_event " +
-                "    WHERE timestmp > " + currentTs +
-                " ORDER BY timestmp asc" +
-                "    LIMIT "+limit;
-        try {
-            Connection conn = fetchDataSource().getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()) {
-                nextTs = rs.getLong("TIMESTMP");
-            }
-        } catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return nextTs;
-    }
-
+    // ----------------------------------------------------------------------------------------------------------------- DISPLAY
     static String SEP="|";
     static String SPC = " "+SEP+" ";
     static int SPC_WIDTH = 3;
@@ -204,6 +106,7 @@ public class ShowLogBackViewPort {
         return field.toString()+SPC;
     }
 
+    // ----------------------------------------------------------------------------------------------------------------- PROPERTIES
     private static ConfigurationStruct fetchProperties(String filename) {
         if(filename==null) filename="../../../resources/logback.properties";
             else filename+="../../../";
@@ -243,6 +146,7 @@ public class ShowLogBackViewPort {
         return struct;
     }
 
+    // ----------------------------------------------------------------------------------------------------------------- KEYBOARD
     private static KeyCode readKey() {
 
         try {
@@ -298,6 +202,106 @@ public class ShowLogBackViewPort {
             e.printStackTrace();
         }
         return KeyCode.KEY_UNKNOWN;
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------- DATABASE
+    private static Map<Long, LogbackTableStruct> fetchRows(Long currentTs, int linesToShow) {
+
+        Map<Long, LogbackTableStruct> map = new TreeMap<Long,LogbackTableStruct>();
+        String sql = "   SELECT timestmp, " +
+                "          formatted_message, " +
+                "          logger_name, " +
+                "          level_string, " +
+                "          thread_name, " +
+                "          reference_flag, " +
+                "          arg0, " +
+                "          arg1, " +
+                "          arg2, " +
+                "          arg3, " +
+                "          caller_filename, " +
+                "          caller_class, " +
+                "          caller_method, " +
+                "          caller_line, " +
+                "          event_id " +
+                "     FROM logging_event ";
+        if(currentTs!=null) {
+            sql +=   "    WHERE timestmp <= " + currentTs;
+        }
+        sql +=       " ORDER BY timestmp desc" +
+                "    LIMIT " + linesToShow;
+        System.out.println(sql);
+
+        try {
+            Connection conn = fetchDataSource().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            LogbackTableStruct struct;
+            while(rs.next()) {
+                struct = new LogbackTableStruct(rs.getLong("TIMESTMP"),
+                        rs.getString("FORMATTED_MESSAGE"),
+                        rs.getString("LOGGER_NAME"),
+                        rs.getString("LEVEL_STRING"),
+                        rs.getString("THREAD_NAME"),
+                        rs.getShort("REFERENCE_FLAG"),
+                        rs.getString("ARG0"),
+                        rs.getString("ARG1"),
+                        rs.getString("ARG2"),
+                        rs.getString("ARG3"),
+                        rs.getString("CALLER_FILENAME"),
+                        rs.getString("CALLER_CLASS"),
+                        rs.getString("CALLER_METHOD"),
+                        rs.getString("CALLER_LINE"),
+                        rs.getInt("EVENT_ID"));
+
+                map.put(struct.ts, struct);
+            }
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return map;
+    }
+
+    private static Long getPreviousTs(Long currentTs, int limit) {
+        Long previousTs=null;
+        String sql = "   SELECT timestmp " +
+                "     FROM logging_event " +
+                "    WHERE timestmp < " + currentTs +
+                " ORDER BY timestmp desc" +
+                "    LIMIT "+limit;
+        try {
+            Connection conn = fetchDataSource().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                previousTs = rs.getLong("TIMESTMP");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return previousTs;
+    }
+
+    private static Long getNextTs(Long currentTs, int limit) {
+        Long nextTs=null;
+        String sql = "   SELECT timestmp " +
+                "     FROM logging_event " +
+                "    WHERE timestmp > " + currentTs +
+                " ORDER BY timestmp asc" +
+                "    LIMIT "+limit;
+        try {
+            Connection conn = fetchDataSource().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                nextTs = rs.getLong("TIMESTMP");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return nextTs;
     }
 
     private static DataSource fetchDataSource() {
