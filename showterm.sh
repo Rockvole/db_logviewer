92f2115d35125075d4d6fa5ec444fc36467cbcbf
Rockvole <rockvole@gmail.com>
2015-11-08 08:49:38 -0800

    Initial

---
+++ showterm.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+root_log=/work/projects/intellij/db_logviewer
+main=build/classes/main
+
+cd $root_log/$main
+echo $PWD
+export CLASSPATH=$root_log/$main/.:$root_log/lib/*:$root_log/lib_gradle/*
+
+unset DISPLAY
+java com.rockvole.logback.ShowLogBackViewPort "$@" 2>&1 | tee /work/tmp/rockvole.log
